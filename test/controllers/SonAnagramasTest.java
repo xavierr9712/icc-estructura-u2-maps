35e130f5cee85597cb63c53983212548b62706f6
Pablo Torers <pabloa_ec@hotmail.com>
2025-07-08 01:20:38 -0500

    ADD: unit tests and placeholders

---
+++ test/controllers/SonAnagramasTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import controllers.Ejercicios;
+
+public class SonAnagramasTest {
+    private final Ejercicios ejercicios = new Ejercicios();
+
+    @Test
+    void testAnagramas() {
+        assertTrue(ejercicios.sonAnagramas("roma", "amor"));
+        assertFalse(ejercicios.sonAnagramas("hola", "halo"));
+    }
+}
